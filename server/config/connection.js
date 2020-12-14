6dd72e0313f669caf30ca08436b8fb0de8d8767f
Gorm <gdsoucie@gmail.com>
2020-12-14 12:17:36 -0500

    attempting to link to atlas db, ran build

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,23 @@
 const mongoose = require('mongoose');
 
+
+
+/*
+const connection = "mongodb+srv://username:<password>@<cluster>/<database>?retryWrites=true&w=majority";
+mongoose.connect(connection,{ useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
+    .then(() => console.log("Database Connected Successfully"))
+    .catch(err => console.log(err));
+    ~~~
+    
+*/
+
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/fixmyship', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
   useFindAndModify: true,
-});
+})
+.then(() => console.log("Database Connected Successfully"))
+.catch(err => console.log(err));
 
 module.exports = mongoose.connection;
